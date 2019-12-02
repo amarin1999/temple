f0243a19b81ed6019774976d88464f2469dc0c7d
amarin1999 <>
2019-12-02 17:46:47 +0700

    develop

--- temple-backend/src/main/java/com/cdgs/temple/TempleApiApplication.java
+++ temple-backend/src/main/java/com/cdgs/temple/TempleApiApplication.java
@@ -1,5 +1,9 @@
 package com.cdgs.temple;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -10,17 +14,28 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.zaxxer.hikari.HikariDataSource;
+
+
 @SpringBootApplication
-public class TempleApiApplication extends SpringBootServletInitializer {
+public class TempleApiApplication implements CommandLineRunner {
 
 	
-	public TempleApiApplication() {
-		super();
-		setRegisterErrorPageFilter(false);
-	}
+	@Autowired
+	DataSource dataSource;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TempleApiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Datasources"+ dataSource);
+		HikariDataSource dataSoxe = (HikariDataSource)dataSource;
+		System.out.println("Datasource"+ dataSoxe.getMaximumPoolSize());
+	}
+	
+	
 	
 //	@Bean
 //	CorsConfigurationSource corsConfigurationSource() {
--- temple-fontend/src/app/shared/constants/ApiConstants.ts
+++ temple-fontend/src/app/shared/constants/ApiConstants.ts
@@ -1,8 +1,8 @@
 export class ApiConstants {
     public static get baseURl(): string {
         //return 'http://157.179.133.0:3999/api/v1';
-        return 'http://localhost:8080/TempleAPI/v1';
-        // return 'http://137.116.149.159/TempleAPI-0.0.1/v1';
+        // return 'http://localhost:8080/TempleAPI/v1';
+        return 'http://137.116.149.159/TempleAPI/v1';
         // return 'http://localhost:8080/TempleAPI-0.0.1/v1';
     }
 }
