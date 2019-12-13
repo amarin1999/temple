a878b26d06641dbfec747e8f79be89bdf1623958
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-13 16:50:29 +0700

    Create history-dharma.service.ts

---
+++ temple-fontend/src/app/shared/service/history-dharma.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiConstants } from '../constants/ApiConstants';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoryDharmaService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getHistoryDharmaByMemberId(memberId) {
+    const body = {
+      memberId: parseInt(memberId)
+    };
+    return this.http.post(ApiConstants.baseURl + `/historydharma/getByMemberId`, body, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access-token')}`
+      }
+    }).pipe(
+      map((res) => {
+        return {
+          status: res['result'],
+          data: res['data'],
+          errorMessage: res['errorMessage']
+        };
+      })
+    );
+  }
+
+  getAllHistoryDharma() {
+    return this.http.get(ApiConstants.baseURl + `/historydharma`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('access-token')}`
+      }
+    }).pipe(
+      map((res) => {
+        return {
+          status: res['result'],
+          data: res['data'],
+          errorMessage: res['errorMessage']
+        };
+      })
+    );
+  }
+}
