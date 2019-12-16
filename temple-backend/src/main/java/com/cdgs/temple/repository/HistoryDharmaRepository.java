df7e0ed7dd5bad1bd4f3f7f8a4ae04c7a40c7726
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-16 14:04:28 +0700

    Update HistoryDharmaRepository.java

--- temple-backend/src/main/java/com/cdgs/temple/repository/HistoryDharmaRepository.java
+++ temple-backend/src/main/java/com/cdgs/temple/repository/HistoryDharmaRepository.java
@@ -2,6 +2,9 @@ package com.cdgs.temple.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -18,5 +21,11 @@ public interface HistoryDharmaRepository extends  CrudRepository<HistoryDharmaEn
 			+ "FROM history_dharma "
 			+ "WHERE history_dharma_member_id = :memberId", nativeQuery = true)
 	List<HistoryDharmaEntity> getHistoryDhamaByMemberId(@Param("memberId") Long memberId);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "DELETE FROM history_dharma "
+			+ "WHERE history_dharma_id = :delId", nativeQuery = true)
+	Integer delHistoryDhamaByMemberId(@Param("delId") Long delId);
 
 }
