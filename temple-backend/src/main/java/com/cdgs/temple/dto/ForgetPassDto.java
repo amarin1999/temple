a72a0343179c60e873ffd7b2ac3ca2aa2c6ae89c
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-18 17:17:16 +0700

    Update ForgetPassDto.java

--- temple-backend/src/main/java/com/cdgs/temple/dto/ForgetPassDto.java
+++ temple-backend/src/main/java/com/cdgs/temple/dto/ForgetPassDto.java
@@ -8,31 +8,28 @@ public class ForgetPassDto implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -1853716198256100356L;
-	private String email;
     private String username;
-    private Long userId;
+    private String idCard;
+    private String phoneNumber;
     
-    public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
+    
 	public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getIdCard() {
+		return idCard;
+	}
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+    
 }
