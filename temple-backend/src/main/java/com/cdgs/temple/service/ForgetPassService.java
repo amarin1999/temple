1bfaea7659da9b2e68c97c155bcbbcf95a55512a
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-18 17:17:00 +0700

    Update ForgetPassService.java

--- temple-backend/src/main/java/com/cdgs/temple/service/ForgetPassService.java
+++ temple-backend/src/main/java/com/cdgs/temple/service/ForgetPassService.java
@@ -2,5 +2,5 @@ package com.cdgs.temple.service;
 
 public interface ForgetPassService {
 		
-	Integer countUser(String email, String userName);
+	Integer countUser(String userName, String idCard, String phoneNumber);
 }
