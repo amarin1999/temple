0eeeeb9278037aa2a4201a8e37ed7f4d77b1b9ab
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-18 17:17:18 +0700

    Create re-password.component.ts

---
+++ temple-fontend/src/app/auth/re-password/re-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-re-password',
+  templateUrl: './re-password.component.html',
+  styleUrls: ['./re-password.component.scss']
+})
+export class RePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
