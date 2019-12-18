e3ce69925c153265aee7737fd6fd356c7b7a7e92
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-18 17:16:53 +0700

    Update forget-password.component.ts

--- temple-fontend/src/app/auth/forget-password/forget-password.component.ts
+++ temple-fontend/src/app/auth/forget-password/forget-password.component.ts
@@ -3,91 +3,133 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ForgetPassService } from '../../shared/service/forget-pass.service';
+import { debounceTime, distinctUntilChanged, min } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 
 @Component({
-	selector: 'app-forget-password',
-	templateUrl: './forget-password.component.html',
-	styleUrls: ['./forget-password.component.css']
+    selector: 'app-forget-password',
+    templateUrl: './forget-password.component.html',
+    styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent implements OnInit {
-	form: FormGroup;
-	@Input() formTitle: string;
-	@Output() data = new EventEmitter<Object>();
-	constructor(
-		private forgetPassService: ForgetPassService,
-		private formBuilder: FormBuilder,
-		private spinner: NgxSpinnerService,
-		private messageService: MessageService,
-	) { }
+    form: FormGroup;
+    @Input() formTitle: string;
+    @Output() data = new EventEmitter<Object>();
+    constructor(
+        private forgetPassService: ForgetPassService,
+        private formBuilder: FormBuilder,
+        private spinner: NgxSpinnerService,
+        private messageService: MessageService,
+        private router: Router
+    ) { }
 
-	ngOnInit() {
-		this.createForm();
-	}
+    public formError = {
+        username: '',
+        idCard: '',
+        phoneNumber: ''
+    };
 
-	onSubmit(e) {
-		this.spinner.show();
-		e.preventDefault();
-		const email = this.form.get('email').value;
-		const username = this.form.get('username').value;
-		// console.log(email, username);
-		if (this.form.valid) {
-			this.spinner.show();
-			this.forgetPassService.getUserForgetInfo(email, username).toPromise().then(res => {
-				// console.log(res['code']);
-				if (res['result'] === 'Success') {
-					if (res['code'] == '200') {
-						// Do something
-						this.messageService.clear();
-						this.messageService.add({
-							key: 'alert',
-							sticky: true,
-							severity: 'success',
-							summary: "ยืนยันการเปลี่ยนรหัสผ่าน"
-						});
-					} else if (res['code'] == '204') {
-						this.messageService.clear();
-						this.messageService.add({
-							key: 'alert',
-							sticky: true,
-							severity: 'warn',
-							summary: 'ไม่มี Email หรือ Username นี้อยู่ในระบบ'
-						});
-					}
-				}
-			}).catch(err => {
-				// Message Not found user or email in system
-				if (err['error']['code'] == 401) {
-					this.messageService.add({
-						key: 'alert',
-						sticky: true,
-						severity: 'error',
-						summary: err['error']['errorMessage']
-					});
-				} else {
-					this.messageService.add({
-						key: 'alert',
-						sticky: true,
-						severity: 'error',
-						summary: 'ระบบขัดข้อง โปรดติดต่อผู้ดูแลระบบ'
-					});
-				}
-			}).finally(() => this.spinner.hide());
-			this.data.emit({ email, username })
-		} else {
-			this.spinner.hide();
-		}
-		
-	}
+    public validationMessage = {
+        username: {
+            // detail: 'กรุณากรอก Username',
+            required: 'ชื่อผู้ใช้*'
+        },
+        idCard: {
+            required: 'เลขประจำตัวประชาชน*'
+        },
+        phoneNumber: {
+            required: 'เบอร์โทรศัพท์*'
+        }
+    };
 
-	onConfirm() {
-		this.messageService.clear('alert');
-	}
+    ngOnInit() {
+        this.createForm();
+    }
 
-	private createForm() {
-		this.form = this.formBuilder.group({
-			"email": ['', [Validators.required, Validators.email]],
-			"username": ['', Validators.required],
-		})
-	}
+    onSubmit(e) {
+        e.preventDefault();
+        const idCard = this.form.get('idCard').value;
+        const username = this.form.get('username').value;
+        const phoneNumber = this.form.get('phoneNumber').value;
+        if (this.form.invalid) {
+            this.subscribeInputMessageWaring();
+        } else if (this.form.valid) {
+            console.log(idCard, username, phoneNumber);
+            this.forgetPassService.getUserForgetInfo(idCard, username, phoneNumber).toPromise().then(res => {
+                this.spinner.show();
+                console.log(res);
+                if (res['result'] === 'Success') {
+                    if (res['code'] === 200) {
+                        // Do something
+                        this.messageService.clear();
+                        this.messageService.add({
+                            key: 'alert',
+                            sticky: true,
+                            severity: 'success',
+                            summary: 'ยืนยันการเปลี่ยนรหัสผ่าน'
+                        });
+                    } else if (res['code'] === 204) {
+                        this.messageService.clear();
+                        this.messageService.add({
+                            key: 'alertFalse',
+                            sticky: true,
+                            severity: 'warn',
+                            summary: 'ไม่มี Username อยู่ในระบบหรือข้อมูลไม่ตรงกัน'
+                        });
+                    }
+                }
+            }).catch(err => {
+                // Message Not found user or email in system
+                if (err['error']['code'] === 401) {
+                    this.messageService.add({
+                        key: 'alertFalse',
+                        sticky: true,
+                        severity: 'error',
+                        summary: err['error']['errorMessage']
+                    });
+                } else {
+                    this.messageService.add({
+                        key: 'alertFalse',
+                        sticky: true,
+                        severity: 'error',
+                        summary: 'ระบบขัดข้อง โปรดติดต่อผู้ดูแลระบบ'
+                    });
+                }
+            }).finally(() => this.spinner.hide());
+            this.data.emit({ idCard, username, phoneNumber });
+        }
+    }
+
+    onConfirm() {
+        this.messageService.clear();
+        // this.router.navigate(['/auth/re-password']);
+    }
+
+    subscribeInputMessageWaring() {
+        this.form.valueChanges
+            .pipe(debounceTime(500), distinctUntilChanged())
+            .subscribe(() => this.waringMessage());
+        this.waringMessage();
+    }
+    waringMessage() {
+        for (const field of Object.keys(this.formError)) {
+            this.formError[field] = '';
+            const control = this.form.get(field);
+            if ((control &&
+                !control.valid &&
+                this.validationMessage[field].required) &&
+                control.hasError) {
+                this.formError[field] = this.validationMessage[field].required;
+            }
+        }
+    }
+
+    private createForm() {
+        this.form = this.formBuilder.group({
+            'idCard': ['', [Validators.required, Validators.minLength(17)]],
+            'username': ['', [Validators.required, Validators.minLength(6)]],
+            'phoneNumber': ['', [Validators.required, Validators.minLength(8)]],
+        });
+    }
 }
