de0687277e9a6de65e47c618a21a7bf269d09d49
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-19 17:07:32 +0700

    Update ForgetPassServiceImpl.java

--- temple-backend/src/main/java/com/cdgs/temple/service/impl/ForgetPassServiceImpl.java
+++ temple-backend/src/main/java/com/cdgs/temple/service/impl/ForgetPassServiceImpl.java
@@ -1,19 +1,26 @@
 package com.cdgs.temple.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cdgs.temple.repository.ForgetPassRepository;
 import com.cdgs.temple.service.ForgetPassService;
+import com.cdgs.temple.service.MemberService;
 
 @Service
 public class ForgetPassServiceImpl implements ForgetPassService{
 	
-	private ForgetPassRepository forgetpassRepository;
+	private static final Logger log = LoggerFactory.getLogger(ForgetPassService.class);
+	
+	private final ForgetPassRepository forgetpassRepository;
+	private	final MemberService memberService;
 
 	
 	@Autowired
-	public ForgetPassServiceImpl(ForgetPassRepository forgetpassRepository) {
+	public ForgetPassServiceImpl(ForgetPassRepository forgetpassRepository, MemberService memberService) {
+		this.memberService = memberService;
 		this.forgetpassRepository = forgetpassRepository;
 		
 	}
