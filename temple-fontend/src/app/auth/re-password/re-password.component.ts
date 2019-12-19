3d0bba06b6a2d8c97bb5134d46390e0f676adeca
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-19 17:07:37 +0700

    Update re-password.component.ts

--- temple-fontend/src/app/auth/re-password/re-password.component.ts
+++ temple-fontend/src/app/auth/re-password/re-password.component.ts
@@ -1,15 +1,146 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ForgetPassService } from 'src/app/shared/service/forget-pass.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-re-password',
-  templateUrl: './re-password.component.html',
-  styleUrls: ['./re-password.component.scss']
+    selector: 'app-re-password',
+    templateUrl: './re-password.component.html',
+    styleUrls: ['./re-password.component.css']
 })
 export class RePasswordComponent implements OnInit {
+    form: FormGroup;
+    @Input() formTitle: string;
+    @Output() data = new EventEmitter<Object>();
 
-  constructor() { }
+    constructor(
+        private forgetPassService: ForgetPassService,
+        private formBuilder: FormBuilder,
+        public spinner: NgxSpinnerService,
+        private messageService: MessageService,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    public formError = {
+        password: '',
+        rePassword: '',
+    };
+
+    public validationMessage = {
+        password: {
+            required: 'รหัสผ่าน*'
+        },
+        rePassword: {
+            required: 'ยืนยันรหัสผ่าน*'
+        }
+    };
+
+    ngOnInit() {
+        this.createForm();
+        if (!this.forgetPassService.memberData) {
+            this.router.navigate(['/']);
+        }
+    }
+
+    private createForm() {
+        this.form = this.formBuilder.group({
+            'password': ['', [Validators.required, Validators.minLength(6)]],
+            'rePassword': ['', [Validators.required, Validators.minLength(6)]],
+        });
+    }
+    
+    onSubmit(e) {
+        this.messageService.clear();
+        e.preventDefault();
+        const password = this.form.get('password').value;
+        const rePassword = this.form.get('rePassword').value;
+        if (password !== rePassword) {
+            this.messageService.add({
+                key: 'alert',
+                sticky: true,
+                severity: 'warn',
+                summary: 'รหัสผ่านไม่ตรงกัน'
+            });
+        } else if (this.form.invalid) {
+            this.messageService.add({
+                key: 'alert',
+                sticky: true,
+                severity: 'warn',
+                summary: 'กรุณากรอกข้อมูลให้ครบถ้วน'
+            });
+            this.subscribeInputMessageWaring();
+        } else if (this.form.valid) {
+            // console.log('MemberData', this.forgetPassService.memberData.password);
+            this.forgetPassService.changePassword(password).toPromise().then(res => {
+                this.spinner.show();
+                console.log(res['data']);
+                if (res['result'] === 'Success') {
+                    if (res['code'] === 200) {
+                        // Do something
+                        this.messageService.clear();
+                        this.messageService.add({
+                            key: 'alert',
+                            sticky: true,
+                            severity: 'success',
+                            summary: 'ยืนยันการเปลี่ยนรหัสผ่าน'
+                        });
+                        this.forgetPassService.memberData = res['data'];
+                    } else if (res['code'] === 204) {
+                        this.messageService.clear();
+                        this.messageService.add({
+                            key: 'alert',
+                            sticky: true,
+                            severity: 'error',
+                            summary: 'ไม่มีชื่อผู้ใช้อยู่ในระบบหรือข้อมูลไม่ตรงกัน'
+                        });
+                    }
+                }
+            }).catch(err => {
+                // Message Not found user or email in system
+                if (err['error']['code'] === 401) {
+                    this.messageService.add({
+                        key: 'alert',
+                        sticky: true,
+                        severity: 'error',
+                        summary: err['error']['errorMessage']
+                    });
+                } else {
+                    this.messageService.add({
+                        key: 'alert',
+                        sticky: true,
+                        severity: 'error',
+                        summary: 'ระบบขัดข้อง โปรดติดต่อผู้ดูแลระบบ'
+                    });
+                }
+            }).finally(() => this.spinner.hide());
+        }
+    }
+
+    subscribeInputMessageWaring() {
+        this.form.valueChanges
+            .pipe(debounceTime(500), distinctUntilChanged())
+            .subscribe(() => this.waringMessage());
+        this.waringMessage();
+    }
+
+    waringMessage() {
+        for (const field of Object.keys(this.formError)) {
+            this.formError[field] = '';
+            const control = this.form.get(field);
+            if ((control &&
+                !control.valid &&
+                this.validationMessage[field].required) &&
+                control.hasError) {
+                this.formError[field] = this.validationMessage[field].required;
+            }
+        }
+    }
+
+    onConfirm() {
+        this.messageService.clear();
+    }
 
 }
