7319408239e4fb1d837f534f62bb85d2558e6c4c
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-26 16:41:17 +0700

    Create RegionEntity

---
+++ temple-backend/src/main/java/com/cdgs/temple/entity/RegionEntity.java
@@ -0,0 +1,48 @@
+package com.cdgs.temple.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "region")
+@Embeddable
+public class RegionEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4920156610220202638L;
+
+	
+	@Id
+	@Column(name = "region_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long regionId;
+	
+	@Column(name = "region_name")
+	private String regionName;
+
+	public Long getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(Long regionId) {
+		this.regionId = regionId;
+	}
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+	
+}
