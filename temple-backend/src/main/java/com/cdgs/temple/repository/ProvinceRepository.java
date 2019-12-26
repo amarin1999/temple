9fbb35a99e1dfeee50b242e0ffc1ca07b098cc51
waithaya-security <waithaya.ch@mail.wu.ac.th>
2019-12-26 16:43:11 +0700

    Update SQL SELECT Data province Joine Region

--- temple-backend/src/main/java/com/cdgs/temple/repository/ProvinceRepository.java
+++ temple-backend/src/main/java/com/cdgs/temple/repository/ProvinceRepository.java
@@ -2,6 +2,7 @@ package com.cdgs.temple.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.cdgs.temple.entity.ProvinceEntity;
@@ -9,5 +10,9 @@ import com.cdgs.temple.entity.ProvinceEntity;
 public interface ProvinceRepository extends CrudRepository<ProvinceEntity, Long> {
 	
 	List<ProvinceEntity> findAll();
+	
+	@Query(value = "SELECT province.province_id, province.province_desc, province.region_id, region.region_name "
+			+ "FROM province JOIN region ON province.region_id = region.region_id", nativeQuery = true)
+	List<ProvinceEntity> getAll();
 
 }
