ce1918963a7e50d91f36fc76b405bccaadc16df7
Frong <basgm45@gmail.com>
2020-01-28 14:31:54 +0700

    fix style  dashboard

--- temple-fontend/src/app/content/dashboard/dashboard/dashboard.component.ts
+++ temple-fontend/src/app/content/dashboard/dashboard/dashboard.component.ts
@@ -15,11 +15,12 @@ export class DashboardComponent implements OnInit {
   userId: string;
   role: string;
   courseHistory: any;
+
   transportOptions: {};
   genderOptions: {};
   regionOptions: {};
   courseHistoryOption: {};
-  constructor(private dashBoard: DashboardService, private breadCrumbService: BreadcrumbService,private authService: AuthService) { }
+  constructor(private dashBoard: DashboardService, private breadCrumbService: BreadcrumbService, private authService: AuthService) { }
 
   ngOnInit() {
     this.setDataChart();
@@ -32,6 +33,11 @@ export class DashboardComponent implements OnInit {
   }
   private setDataChart() {
     this.dashBoard.getDataChart().subscribe(res => {
+      const sumGender = (res[0].gender.reduce((dataGender, sum) => sum + dataGender));
+      const sumTransport = (res[0].transport.reduce((dataTransport, sum) => sum + dataTransport));
+      const sumRegion = (res[0].region.reduce((dataRegion, sum) => sum + dataRegion));
+      const sumCourse = (res[0].courseHistory.reduce((dataCourse, sum) => sum +dataCourse));
+
       this.gender = {
         labels: ['ชาย', 'หญิง', 'ไม่ระบุ'],
         datasets: [
@@ -50,114 +56,125 @@ export class DashboardComponent implements OnInit {
           }]
       };
       this.transport = {
-        labels: [''],
+        labels: ['การเดินทางของวัด' , 'การเดินทางมาด้วยตนเอง'],
         datasets: [
           {
-            label: 'การเดินทางของวัด',
-            backgroundColor: '#42A5F5',
-            borderColor: '#1E88E5',
-            data: [res[0].transport[0]]
+            data: res[0].transport,
+            backgroundColor: [
+              '#42A5F5',
+              '#9CCC65'
+            ],
+            hoverBackgroundColor: [
+              '#42A5F5',
+              '#9CCC65'
+            ]
+          }
+        ]
+      },
+      this.region = {
+        labels: [''],
+        // 'กลาง', 'ตะวันออกเฉียงเหนือ', 'เหนือ', 'ใต้', 'ตะวันออก', 'ตะวันตก'
+        datasets: [
+          {
+            label: 'กลาง',
+            data: [res[0].region[0]],
+            backgroundColor: '#ff6d6d'
           },
           {
-            label: 'การเดินทางมาด้วยตนเอง',
-            backgroundColor: '#9CCC65',
-            borderColor: '#7CB342',
-            data: [res[0].transport[1]]
+            label: 'ตะวันออกเฉียงเหนือ',
+            data: [res[0].region[1]],
+            backgroundColor: '#fff766'
+          },
+          {
+            label: 'เหนือ',
+            data: [res[0].region[2]],
+            backgroundColor: '#5cff8a'
+          },
+          {
+            label: 'ใต้',
+            data: [res[0].region[3]],
+            backgroundColor: '#73fffa'
+          },
+          {
+            label: 'ตะวันออก',
+            data: [res[0].region[4]],
+            backgroundColor: '#d16eff'
+          },
+          {
+            label: 'ตะวันตก',
+            data: [res[0].region[5]],
+            backgroundColor: '#36A2EB'
           }
         ]
-      }
-      this.region = {
-        labels: ['กลาง', 'ตะวันออกเฉียงเหนือ', 'เหนือ', 'ใต้', 'ตะวันออก', 'ตะวันตก'],
-        datasets: [
-          {
-            data: res[0].region,
-            backgroundColor: [
-              '#ff6d6d',
-              '#fff766',
-              '#5cff8a',
-              '#73fffa',
-              '#d16eff',
-              '#36A2EB',
-            ],
-            hoverBackgroundColor: [
-              '#ff6d6d',
-              '#fff766',
-              '#5cff8a',
-              '#73fffa',
-              '#d16eff',
-              '#36A2EB',
-            ]
-          }]
-      }
+        },
       this.courseHistory = {
-        labels: ['คอร์สที่ไม่ผ่านหลักสูตร (F)', 'คอร์สที่ผ่านหลักสูตร', 'คอร์สที่กำลังศึกษาอยู่'],
-        datasets: [
-          {
-            data: res[0].courseHistory,
-            backgroundColor: [
-              '#ff6d6d',
-              '#52DE22',
-              '#F3EF18'
-            ],
-            hoverBackgroundColor: [
-              '#ff6d6d',
-              '#52DE22',
-              '#F3EF18'
-            ]
-          }
-        ]
-      };
-    });
-
-    this.transportOptions = {
-      title: {
-        display: true,
-        text: 'ช่องทางการเดินทางของผู้เข้าอบรม',
-        fontSize: 16,
-        position: 'bottom'
-      }
-    };
-
-
-    this.genderOptions = {
-      title: {
-        display: true,
-        text: 'เพศของผู้เข้าอบรม',
-        fontSize: 16,
-        position: 'bottom'
-      }
-    };
-
-    this.regionOptions = {
-      title: {
-        display: true,
-        text: 'ภูมิภาคของผู้เข้าอบรม',
-        fontSize: 16,
-        position: 'bottom'
-      }
-    };
-  this.courseHistoryOption = {
-    title: {
-        display: true,
-        text: 'คอร์สทั้งหมดที่ทำการสมัครเรียน',
-        fontSize: 16,
-        position: 'bottom'
+  labels: ['คอร์สที่ไม่ผ่านหลักสูตร (F)', 'คอร์สที่ผ่านหลักสูตร', 'คอร์สที่กำลังศึกษาอยู่'],
+  datasets: [
+    {
+      data: res[0].courseHistory,
+      backgroundColor: [
+        '#ff6d6d',
+        '#52DE22',
+        '#F3EF18'
+      ],
+      hoverBackgroundColor: [
+        '#ff6d6d',
+        '#52DE22',
+        '#F3EF18'
+      ]
     }
+  ]
+};
+this.transportOptions = {
+  title: {
+    display: true,
+    text: `ช่องทางการเดินทางของผู้เข้าอบรม (ทั้งหมด ${sumTransport} คน)`,
+    fontSize: 16,
+    position: 'bottom'
   }
+};
+
+
+this.genderOptions = {
+  title: {
+    display: true,
+    text: `เพศของผู้เข้าอบรม (ทั้งหมด ${sumGender} คน)`,
+    fontSize: 16,
+    position: 'bottom'
+  }
+};
+
+this.regionOptions = {
+  title: {
+    display: true,
+    text: `ภูมิภาคของผู้เข้าอบรม (ทั้งหมด ${sumRegion} คน)`,
+    fontSize: 16,
+    position: 'bottom'
+  }
+};
+this.courseHistoryOption = {
+  title: {
+    display: true,
+    text: `คอร์สทั้งหมดที่ทำการสมัครเรียน (ทั้งหมด ${sumCourse} คอร์ส)`,
+    fontSize: 16,
+    position: 'bottom'
+  }
+}
+    });
 
 
 
   };
 
-  showDashboardRole(...role) {
-    return role.includes(this.role);
-  }
-  private getUserId(){
-    this.userId = localStorage.getItem('userId');
-  }
-  private getRole(){
-    this.authService.getRole().subscribe(res => this.role = res);
-  }
-  
+showDashboardRole(...role) {
+  return role.includes(this.role);
+}
+  private getUserId() {
+  this.userId = localStorage.getItem('userId');
+}
+  private getRole() {
+  this.authService.getRole().subscribe(res => this.role = res);
+}
+
 }
 
