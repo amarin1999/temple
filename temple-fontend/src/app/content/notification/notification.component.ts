ebe690d73d0e1e4cf5bd52a8b95bb018dd093455
Frong <basgm45@gmail.com>
2020-01-30 10:26:39 +0700

    Hide notification

--- temple-fontend/src/app/content/notification/notification.component.ts
+++ temple-fontend/src/app/content/notification/notification.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
+  notiDisable: boolean = true;
+  currentUrl: string;
 
-  constructor() { }
+  // เรียก current path เพื่อซ่อนการแจ้งเตือน
+  constructor(private router: Router) {
+    this.router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+  }
 
   ngOnInit() {
+  }
+  toggle() {
+    this.notiDisable = false;
   }
 
 
