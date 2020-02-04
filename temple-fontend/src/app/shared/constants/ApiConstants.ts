1e5991b8af54cf3b52af59ac7fcf517f4b118895
devilbas <devilbastopoftheworld@gmail.com>
2020-02-04 09:21:22 +0700

    Update ApiConstants.ts

--- temple-fontend/src/app/shared/constants/ApiConstants.ts
+++ temple-fontend/src/app/shared/constants/ApiConstants.ts
@@ -2,5 +2,6 @@ export class ApiConstants {
     public static get baseURl(): string {
         return 'http://localhost:8080/TempleAPI/v1';
         // return 'http://13.76.96.231/TempleAPI/v1';
+        // return 'http://20.191.137.58/TempleAPI/v1';
     }
 }
