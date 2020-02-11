b597947505e06ea6d3b4bc793be0f0623f36fe65
Ppich00 <49331622+Ppich00@users.noreply.github.com>
2020-02-11 09:06:51 +0700

    edit API

--- temple-fontend/src/app/shared/constants/ApiConstants.ts
+++ temple-fontend/src/app/shared/constants/ApiConstants.ts
@@ -1,6 +1,9 @@
+import { environment } from 'src/environments/environment';
+
+const API = environment.api;
 export class ApiConstants {
     public static get baseURl(): string {
         //ใช้ environment PROD/DEV URL 
-        return "http://localhost:8080/TempleAPI/v1";
+        return API;
     }
 }
