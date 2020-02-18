a2aac55c6bc6c42c69564bb9eb057f9a869f8359
59122201005 <s59122201005@ssru.ac.th>
2020-02-18 10:50:21 +0700

    sms

--- temple-backend/src/main/java/com/cdgs/temple/service/impl/SmsServiceImpl.java
+++ temple-backend/src/main/java/com/cdgs/temple/service/impl/SmsServiceImpl.java
@@ -1,13 +1,45 @@
 package com.cdgs.temple.service.impl;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 import com.cdgs.temple.service.SmsService;
+
+import ClickSend.ApiClient;
+import ClickSend.ApiException;
+import ClickSend.Api.SmsApi;
+import ClickSend.Model.SmsMessage;
+import ClickSend.Model.SmsMessageCollection;
+
+
 
 public class SmsServiceImpl implements SmsService {
 	static final String USERNAME = "TempleAPIv123456@gmail.com";
-	static final String PASSWORD = "Temple123456789+";
+	static final String API_KEY = "EFEC314F-7686-5F2D-A87B-E389F8486022";
 	
 	@Override
 	public void sendSms(String fromPhoneNo, String toPhoneNo, String messageText) {
-		
+		ApiClient defaultClient = new ApiClient();
+	    defaultClient.setUsername(USERNAME);
+	    defaultClient.setPassword(API_KEY);
+	    SmsApi apiInstance = new SmsApi(defaultClient);
+
+	    SmsMessage smsMessage=new SmsMessage();
+	    smsMessage.setBody(messageText);
+	    smsMessage.setTo(toPhoneNo);
+	    smsMessage.setSource("sdk");
+
+	    List<SmsMessage> smsMessageList=Arrays.asList(smsMessage);
+	    // SmsMessageCollection | SmsMessageCollection model
+	    SmsMessageCollection smsMessages = new SmsMessageCollection();
+	    smsMessages.messages(smsMessageList);
+	    try {
+	        String result = apiInstance.smsSendPost(smsMessages);
+	        System.out.println(result);
+	    } catch (ApiException e) {
+	        System.err.println("Exception when calling SmsApi#smsSendPost");
+	        e.printStackTrace();
+	    }
 	}
 }
