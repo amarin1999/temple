97ccdef420ec2bd201f227e3991956cebcd9ffcd
devilbas <devilbastopoftheworld@gmail.com>
2020-02-20 15:30:32 +0700

    add log in smsserviceimpl

--- temple-backend/src/main/java/com/cdgs/temple/service/impl/SmsServiceImpl.java
+++ temple-backend/src/main/java/com/cdgs/temple/service/impl/SmsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.cdgs.temple.service.impl;
 
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,35 +10,34 @@ import ClickSend.ApiException;
 import ClickSend.Api.SmsApi;
 import ClickSend.Model.SmsMessage;
 import ClickSend.Model.SmsMessageCollection;
+import lombok.extern.slf4j.Slf4j;
 
-
-
+@Slf4j
 public class SmsServiceImpl implements SmsService {
 	static final String USERNAME = "TempleAPIv123456@gmail.com";
 	static final String API_KEY = "EFEC314F-7686-5F2D-A87B-E389F8486022";
-	
+
 	@Override
-	public void sendSms(String fromPhoneNo, String toPhoneNo, String messageText) {
+	public void sendSms(String toPhoneNo, String messageText) {
 		ApiClient defaultClient = new ApiClient();
-	    defaultClient.setUsername(USERNAME);
-	    defaultClient.setPassword(API_KEY);
-	    SmsApi apiInstance = new SmsApi(defaultClient);
+		defaultClient.setUsername(USERNAME);
+		defaultClient.setPassword(API_KEY);
+		SmsApi apiInstance = new SmsApi(defaultClient);
 
-	    SmsMessage smsMessage=new SmsMessage();
-	    smsMessage.setBody(messageText);
-	    smsMessage.setTo(toPhoneNo);
-	    smsMessage.setSource("sdk");
+		SmsMessage smsMessage = new SmsMessage();
+		smsMessage.setBody(messageText);
+		smsMessage.setTo(toPhoneNo);
+		smsMessage.setSource("sdk");
 
-	    List<SmsMessage> smsMessageList=Arrays.asList(smsMessage);
-	    // SmsMessageCollection | SmsMessageCollection model
-	    SmsMessageCollection smsMessages = new SmsMessageCollection();
-	    smsMessages.messages(smsMessageList);
-	    try {
-	        String result = apiInstance.smsSendPost(smsMessages);
-	        System.out.println(result);
-	    } catch (ApiException e) {
-	        System.err.println("Exception when calling SmsApi#smsSendPost");
-	        e.printStackTrace();
-	    }
+		List<SmsMessage> smsMessageList = Arrays.asList(smsMessage);
+		// SmsMessageCollection | SmsMessageCollection model
+		SmsMessageCollection smsMessages = new SmsMessageCollection();
+		smsMessages.messages(smsMessageList);
+		try {
+			String result = apiInstance.smsSendPost(smsMessages);
+			log.info(result);
+		} catch (ApiException e) {
+			log.error("Exception when calling SmsApi#smsSendPost", e);
+		}
 	}
 }
